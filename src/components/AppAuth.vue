1ab525fa242b1ed9f9d6d51efce6682ab74c8f4c
akhfih <akhfih.dev@gmail.com>
2023-01-18 22:04:06 +0700

    feat: validate numbers

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -116,10 +116,12 @@
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input
+              <vee-field
                 type="number"
+                name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               />
+              <ErrorMessage class="text-red-600" name="age" />
             </div>
             <!-- Password -->
             <div class="mb-3">
@@ -175,13 +177,13 @@ import { mapState, mapWritableState } from "pinia";
 import useModalStore from "@/stores/modal";
 export default {
   name: "AppAuth",
-  data: function () {
+  data() {
     return {
       tab: "login",
       schema: {
         name: "required|min:3|max:100|alpha_spaces",
         email: "required|min:3|max:100|email",
-        age: "",
+        age: "required|min_value:18|max_value:100",
         password: "",
         confirm_password: "",
         country: "",
