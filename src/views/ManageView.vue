c61a2cd3550fd297ddd1da53f9a9bfc197d61615
akhfih <akhfih.dev@gmail.com>
2023-02-12 15:14:28 +0700

    feat: updating the list of songs after an Upload

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -3,7 +3,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <app-upload ref="upload" />
+        <app-upload ref="upload" :addSong="addSong" />
       </div>
       <div class="col-span-2">
         <div
@@ -52,13 +52,7 @@ export default {
     const snapshot = await songsCollection
       .where("uid", "==", auth.currentUser.uid)
       .get();
-    snapshot.forEach((document) => {
-      const song = {
-        ...document.data(),
-        docID: document.id,
-      };
-      this.songs.push(song);
-    });
+    snapshot.forEach(this.addSong);
   },
   methods: {
     updateSong(i, values) {
@@ -67,6 +61,13 @@ export default {
     },
     removeSong(i) {
       this.songs.splice(i, 1);
+    },
+    addSong(document) {
+      const song = {
+        ...document.data(),
+        docID: document.id,
+      };
+      this.songs.push(song);
     },
   },
   // beforeRouteLeave(to, from, next) {
