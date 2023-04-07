52d6b452a87fe2c95ec20fe6142ca21e750feb81
akhfih <akhfih.dev@gmail.com>
2023-04-07 09:02:36 +0700

    feat: duration and current position

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,41 +1,54 @@
 import { defineStore } from "pinia";
 import { Howl } from "howler";
 
-export default defineStore("player",  {
-    state: () => ({
-        current_song: {},
-        sound: {},
-    }),
-    actions: {
-        async newSong(song) {
-            this.current_song = song;
+export default defineStore("player", {
+  state: () => ({
+    current_song: {},
+    sound: {},
+    seek: "00:00",
+    duration: "00.00",
+  }),
+  actions: {
+    async newSong(song) {
+      this.current_song = song;
 
-            this.sound = new Howl({
-                src: [song.url],
-                html5: true,
-            });
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true,
+      });
 
-            this.sound.play();
-        },
-        async toggleAudio() {
-            if(!this.sound.playing) {
-                return;
-            }
+      this.sound.play();
 
-            if(this.sound.playing()){
-                this.sound.pause();
-            }else {
-                this.sound.play();
-            }
-        },
-        
+      this.sound.on("play", () => {
+        requestAnimationFrame(this.progress);
+      });
     },
-    getters: {
-        playing: (state) => {
-            if (state.sound.playing){
-                return state.sound.playing();
-            }
-            return false;
-        }
-    }
-})
+    async toggleAudio() {
+      if (!this.sound.playing) {
+        return;
+      }
+
+      if (this.sound.playing()) {
+        this.sound.pause();
+      } else {
+        this.sound.play();
+      }
+    },
+    progress() {
+      this.seek = this.sound.seek();
+      this.duration = this.sound.duration();
+
+      if (this.sound.playing()) {
+        requestAnimationFrame(this.progress);
+      }
+    },
+  },
+  getters: {
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
+  },
+});
