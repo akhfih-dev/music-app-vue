8218bbaf140350c55ef32b6967922a272aee6914
akhfih <akhfih.dev@gmail.com>
2023-04-08 18:40:22 +0700

    feat: changing locales

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -37,6 +37,13 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2 text-white" href="#" @click.prevent="changeLocale">{{
+              currentLocale
+            }}</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -49,6 +56,9 @@ export default {
   name: "AppHeader",
   computed: {
     ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === "id" ? "Indonesia" : "English";
+    },
   },
   methods: {
     toggleAuthModal() {
@@ -60,6 +70,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: "home" });
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === "id" ? "en" : "id";
     },
   },
 };
